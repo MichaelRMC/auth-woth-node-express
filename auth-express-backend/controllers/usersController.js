d218da3e350c217e895b17da57b64af8535782cb
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-12-18 22:31:17 -0500

    Add queries

--- auth-express-backend/controllers/usersController.js
+++ auth-express-backend/controllers/usersController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const {
   getOneUser,
@@ -11,12 +11,37 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const users = express.Router();
 
 // LOGIN ROUTE
-users.post("/login", async (req, res) => {
-
+users.post( "/login", async ( req, res ) =>
+{
+  try {
+    const {email, password} = req.body
+    const login = await getOneUserByEmail( email )
+    const checkPassword = bcrypt.compare( password, users.password )
+      if (login === !users.email && checkPassword === !users.password) {
+        return res.status(403).json({message:"Invalid Entry"})
+    } 
+    //This is where the token would be generated.
+  } catch (error) {
+    res.status(500).json({Error: 'Server Error'})
+  }
 });
 
 // SIGN UP ROUTE
-users.post("/", async (req, res) => {
+users.post( "/", async ( req, res ) =>
+{
+  try {
+    const { firstname, lastname, email, password } = req.body;
+    if (!firstname,!lastname,!email && !password) {
+      return res.status(403).json({Error: "All Fields are required!"})
+    }
+    const saltRounds = await bcrypt.genSalt( 10 );
+    const hashedPassword = await bcrypt.hash( password, saltRounds );
+    const newUser = await createUser( { firstname, lastname, email, hashedPassword } );
+    return res.status(201).json({message: "Congratulations, your account has been successfully created!"})
+    
+  } catch (error) {
+    
+  }
   
 });
 
--- auth-express-backend/queries/users.js
+++ auth-express-backend/queries/users.js
@@ -5,13 +5,34 @@ const db = require("../db/dbConfig.js");
 
 
 const getOneUser = async (id) => {
-
+    try {
+        const user = await db.one( "SELECT * FROM users WHERE id=$1", id )
+        return user
+    } catch (error) {
+        console.error(error)
+    }
 };
-const getOneUserByEmail = async ({email}) => {
-
+const getOneUserByEmail = async ( { email } ) =>
+{
+    try {
+        const oneUser = await db.one( 'SELECT * FROM users WHERE email = $1', email )
+        return oneUser
+    } catch (error) {
+        return console.error( error );
+    }
+    
 };
 const createUser = async (user) => {
-
+    try {
+        const { firstname, lastname, email, password } = user
+        const createdUser = await db.one(
+            'INSERT INTO users (firstname, lastname, email, password ) VALUES ($1, $2, $3, $4) RETURNING *',
+            [ firstname, lastname, email, password ]
+        );
+        return createdUser
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 
